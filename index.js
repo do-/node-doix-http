d04e7ded4aeb29c8f0872d984b6ec99875c90080
Dmitry Ovsyanko <do_@pochta.ru>
2023-01-01 12:30:42 +0300

    HttpStaticSite

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
 	HttpRequestContext: require ('./lib/HttpRequestContext.js'),
 	HttpRouter: require ('./lib/HttpRouter.js'),
 	HttpJobSource: require ('./lib/HttpJobSource.js'),
+	HttpStaticSite: require ('./lib/HttpStaticSite.js'),
 	HttpParamReader: require ('./lib/HttpParamReader.js'),
 }
