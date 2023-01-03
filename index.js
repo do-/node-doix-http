c4663c748f3e6b24e6a6644870987b7b4c2ad91b
Dmitry Ovsyanko <do_@pochta.ru>
2023-01-03 16:07:45 +0300

    HttpJobSource renamed to WebService

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
 	HttpRequestContext: require ('./lib/HttpRequestContext.js'),
 	HttpRouter: require ('./lib/HttpRouter.js'),
-	HttpJobSource: require ('./lib/HttpJobSource.js'),
+	WebService: require ('./lib/WebService.js'),
 	HttpStaticSite: require ('./lib/HttpStaticSite.js'),
 	HttpParamReader: require ('./lib/HttpParamReader.js'),
 	HttpResultWriter: require ('./lib/HttpResultWriter.js'),
