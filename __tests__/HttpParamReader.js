36c101fbafe2dddf5768cbf4077241aadb22b42a
Dmitry Ovsyanko <do_@pochta.ru>
2023-06-03 21:25:26 +0300

    __tests__/HttpParamReader.js

---
+++ __tests__/HttpParamReader.js
@@ -0,0 +1,25 @@
+const {HttpParamReader} = require ('..')
+const {ObjectMerger} = require ('doix')
+
+test ('constructor', () => {
+
+	expect (new HttpParamReader ()).toBeInstanceOf (HttpParamReader)
+
+	expect (new HttpParamReader ({0: undefined, merger: new ObjectMerger ()})).toBeInstanceOf (HttpParamReader)
+
+	expect (() => new HttpParamReader ({from: 0})).toThrow ()
+
+	expect (() => new HttpParamReader ({merger: 0})).toThrow ()
+
+	expect (() => new HttpParamReader ({'': 0})).toThrow ()
+
+})
+
+test ('from', () => {
+
+	const job = {on: x => x, http: {request: {method: 'TRACE'}}}
+
+	new HttpParamReader ().process (job)
+
+})
+
