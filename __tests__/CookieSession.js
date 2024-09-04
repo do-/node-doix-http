605ed39a4c113d4080e650c2feee616119f946b1
Dmitry Ovsyanko <do_@pochta.ru>
2024-09-04 11:40:49 +0300

    CookieSession: name validation

--- __tests__/CookieSession.js
+++ __tests__/CookieSession.js
@@ -4,8 +4,11 @@ test ('rudimentary', async () => {
 
 	expect (() => new CookieSession ()).toThrow ()
 	expect (() => new CookieSession (0)).toThrow ()
+	expect (() => new CookieSession ({})).toThrow ()
+	expect (() => new CookieSession ({name: 0})).toThrow ()
+	expect (() => new CookieSession ({name: ''})).toThrow ()
 
-	const s = new CookieSession ({})
+	const s = new CookieSession ({name: 'sid'})
 
 	expect (await s.getDb ()).toBeUndefined ()
 	expect (await s.storeUser ()).toBeUndefined ()
